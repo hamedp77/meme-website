49d28e0c0b4b302a069a95474f31cd0df5ab8117
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-02-09 15:51:02 +0330

    returning more stuff from meme api to show in html

--- meme_app.py
+++ meme_app.py
@@ -8,13 +8,16 @@ def get_meme():
     meme_endpoint = 'https://meme-api.com/gimme'
     response = requests.get(meme_endpoint).json()
     meme_pic = response['preview'][-1]
+    subreddit = response['subreddit']
+    post_link = response['postLink']
+    title = response['title']
 
-    return meme_pic
+    return meme_pic, subreddit, post_link, title
 
 @app.route('/')
 def index():
-    meme_pic = get_meme()
+    meme_pic, subreddit, post_link, title = get_meme()
 
-    return render_template('index.html', meme_pic=meme_pic)
+    return render_template('index.html', meme_pic=meme_pic, post_link=post_link, subreddit=subreddit, title=title)
 
 app.run(host='0.0.0.0', port=5000)
