3018b1fec444636fb29320561c8a593ed245d890
Hamed Pourkhaleghi <hamedp77@gmail.com>
2023-02-10 14:55:02 +0330

    adding function docstrings

--- meme_app.py
+++ meme_app.py
@@ -4,17 +4,24 @@ import requests
 app = Flask(__name__)
 
 def get_meme():
+    """
+    get a random meme from a random subreddit.
+    returns the highest res preview of the meme.
+    """
 
     meme_endpoint = 'https://meme-api.com/gimme'
     response = requests.get(meme_endpoint).json()
     meme_pic = response['preview'][-1]
-
     return meme_pic
+
 
 @app.route('/')
 def index():
-    meme_pic = get_meme()
+    """rendering the homepage of the website with the meme in it"""
 
+    meme_pic = get_meme()
     return render_template('index.html', meme_pic=meme_pic)
 
-app.run(host='0.0.0.0', port=443)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=443)
